fe5b3e491f487d1dde5a1f6c448e3a39f9095a9e
Dave <uchennadavid2404@gmail.com>
2023-11-15 21:51:53 +0100

    Testing

--- src/server.ts
+++ src/server.ts
@@ -18,13 +18,6 @@ import helmetCsp from 'helmet-csp';
 import hpp from 'hpp';
 import morgan from 'morgan';
 import xss from 'xss-clean';
-import { ENVIRONMENT } from './common/config';
-import { connectDb } from './common/config/database';
-import { logger, stream } from './common/utils/logger';
-import errorHandler from './controllers/errorController';
-import { timeoutMiddleware, validateDataWithZod } from './middlewares';
-import { emailQueue, emailQueueEvent, emailWorker, stopQueue } from './queues/emailQueue';
-import { authRouter, userRouter } from './routes';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
@@ -196,8 +189,8 @@ const server = app.listen(port, async () => {
 	// start the email worker and queues
 	(async () => {
 		await emailQueue.waitUntilReady();
-		await emailWorker.waitUntilReady();
-		await emailQueueEvent.waitUntilReady();
+		// await emailWorker.waitUntilReady();
+		// await emailQueueEvent.waitUntilReady();
 	})();
 });
 
