8c9254cd93dc988ee9d50cfc451f5b8cdac30c97
Bayode Emmanuel <obcbeats@gmail.com>
2023-11-17 14:27:59 +0300

    Update src/controllers/auth/protect.ts
    Co-authored-by: JC CODER <99423608+JC-Coder@users.noreply.github.com>

--- src/controllers/auth/protect.ts
+++ src/controllers/auth/protect.ts
@@ -96,7 +96,7 @@ export const protect = catchAsync(async (req: Request, res: Response, next: Next
 				next(new AppError('Invalid token, please log in again', 500));
 			}
 		} else {
-			next(new AppError('Invalid token, please log in again', 500));
+			next(new AppError('Invalid token, please log in again', 401));
 		}
 	}
 	next();
