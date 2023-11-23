bc9219de6b8b7688532b903e58c82bfe87430073
ToyinM <toyinmuhammed50@gmail.com>
2023-11-23 21:01:21 +0100

    fixed!

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,12 +1,13 @@
-import { protect, resetPassword, session, signIn, signUp, verifyEmail } from '@/controllers';
+import { forgotPassword, protect, resetPassword, session, signIn, signUp, verifyEmail } from '@/controllers';
 import { Router } from 'express';
 
 const router = Router();
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
+router.post('/password/forgot', forgotPassword);
+router.post('/password/reset', resetPassword);
 router.post('/verify-email', verifyEmail);
-router.post('/reset-password', resetPassword);
 
 router.use(protect); // Protect all routes after this middleware
 router.get('/session', session);
