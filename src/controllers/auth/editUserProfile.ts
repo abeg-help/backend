af1ab9d20dd5fd387a867e800a807957e8184b7c
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-11-24 23:56:14 +0100

    Created the edit user profile field1

--- src/controllers/auth/editUserProfile.ts
+++ src/controllers/auth/editUserProfile.ts
@@ -1,18 +1,16 @@
-import { CustomRequest, IUser } from '@/common/interfaces';
-import { setCache, toJSON } from '@/common/utils';
-import AppError from '@/common/utils/appError';
-import { AppResponse } from '@/common/utils/appResponse';
+import { Response } from 'express';
+import { setCache } from '@/common/utils';
 import { catchAsync } from '@/middlewares';
 import { UserModel as User } from '@/models';
-import { Response } from 'express';
+import { CustomRequest, IUser } from '@/common/interfaces';
+import { AppResponse } from '@/common/utils/appResponse';
 
 export const editUserProfile = catchAsync(async (req: CustomRequest, res: Response) => {
 	//collect the details to be updated
 	const { firstName, lastName, phoneNumber, gender } = req.body;
 
 	//get the user id to update from req.user
-	const UserToUpdateID = req.user?._id;
-	if (!UserToUpdateID) throw new AppError('Provide User ID to updated', 400);
+	const UserToUpdateEmail = req.user?._id;
 
 	//Partial makes the objects to update optional while extending the user inteface
 	const objectToUpdate: Partial<IUser> = {
@@ -23,16 +21,11 @@ export const editUserProfile = catchAsync(async (req: CustomRequest, res: Respon
 	};
 
 	//updates the id with object, new returns the updated user while runnung mongoose validation
-	const updatedUser = await User.findByIdAndUpdate({ _id: UserToUpdateID }, objectToUpdate, {
+	const updatedUser = await User.findByIdAndUpdate({ _id: UserToUpdateEmail }, objectToUpdate, {
 		new: true,
 		runValidators: true,
 	});
 
-	if (!updatedUser) {
-		//  no user is found to update
-		return AppResponse(res, 404, null, 'User not found for update');
-	}
-
-	await setCache(`Updated User: ${updatedUser?._id.toString()}`, toJSON(updatedUser, ['password']), 3600);
+	await setCache(`Updated User: ${updatedUser?._id}`, 'supposed to be access token', 3600);
 	AppResponse(res, 200, updatedUser, 'Profile Successfully Updated');
 });
