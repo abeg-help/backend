09c54f1a04984a2ed18cd5b49fbfe933fab8554f
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-11-24 23:56:14 +0100

    Created the edit user profile field1

---
+++ src/controllers/auth/editUserProfile.ts
@@ -0,0 +1,31 @@
+import { Response } from 'express';
+import { setCache } from '@/common/utils';
+import { catchAsync } from '@/middlewares';
+import { UserModel as User } from '@/models';
+import { CustomRequest, IUser } from '@/common/interfaces';
+import { AppResponse } from '@/common/utils/appResponse';
+
+export const editUserProfile = catchAsync(async (req: CustomRequest, res: Response) => {
+	//collect the details to be updated
+	const { firstName, lastName, phoneNumber, gender } = req.body;
+
+	//get the user id to update from req.user
+	const UserToUpdateEmail = req.user?._id;
+
+	//Partial makes the objects to update optional while extending the user inteface
+	const objectToUpdate: Partial<IUser> = {
+		firstName,
+		lastName,
+		phoneNumber,
+		gender,
+	};
+
+	//updates the id with object, new returns the updated user while runnung mongoose validation
+	const updatedUser = await User.findByIdAndUpdate({ _id: UserToUpdateEmail }, objectToUpdate, {
+		new: true,
+		runValidators: true,
+	});
+
+	await setCache(`Updated User: ${updatedUser?._id}`, 'supposed to be access token', 3600);
+	AppResponse(res, 200, updatedUser, 'Profile Successfully Updated');
+});
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,4 +1,5 @@
 import { protect, forgotPassword, resetPassword, session, signIn, signUp } from '@/controllers';
+import { editUserProfile } from '@/controllers/auth/editUserProfile';
 import { Router } from 'express';
 
 const router = Router();
@@ -10,5 +11,6 @@ router.post('/password/reset', resetPassword);
 
 router.use(protect); // Protect all routes after this middleware
 router.get('/session', session);
+router.get('/user/updateProfile', editUserProfile);
 
 export { router as authRouter };
