3f2d837118dbed191e261ffe9e71868d0fe17250
ToyinM <toyinmuhammed50@gmail.com>
2023-11-24 23:43:24 +0100

    final fix?

--- src/controllers/auth/verifyEmail.ts
+++ src/controllers/auth/verifyEmail.ts
@@ -1,24 +1,14 @@
-import { IUser } from '@/common/interfaces';
 import { decodeData, getFromCache, setCache } from '@/common/utils';
 import AppError from '@/common/utils/appError';
 import { AppResponse } from '@/common/utils/appResponse';
 import { catchAsync } from '@/middlewares';
 import { UserModel } from '@/models';
 import { Request, Response } from 'express';
-import { Require_id } from 'mongoose';
 
 export const verifyEmail = catchAsync(async (req: Request, res: Response) => {
 	const { token, userId } = req.body;
 	if (!token || !userId) {
 		throw new AppError('All fields are required!', 400);
-	}
-
-	const cachedUser = await getFromCache(userId);
-
-	const user = cachedUser ? cachedUser : ((await UserModel.findOne({ _id: userId })) as Require_id<IUser>);
-
-	if (!user) {
-		throw new AppError('User not found', 404);
 	}
 
 	const validToken = await getFromCache(`verification:${userId}`);
@@ -33,10 +23,10 @@ export const verifyEmail = catchAsync(async (req: Request, res: Response) => {
 		throw new AppError('Invalid token', 400);
 	}
 
-	const updatedUser = await UserModel.findByIdAndUpdate(user, { isEmailVerified: true }, { new: true });
+	const updatedUser = await UserModel.findByIdAndUpdate(userId, { isEmailVerified: true }, { new: true });
 
 	if (!updatedUser) {
-		throw new AppError('Internal server error', 500);
+		throw new AppError('Authentication error', 400);
 	}
 
 	await setCache(updatedUser._id.toString(), updatedUser.toJSON(['password']));
