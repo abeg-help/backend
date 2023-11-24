1c370e16f8c145ff6d3b8195b539bb886074ee8f
Dave <uchennadavid2404@gmail.com>
2023-11-24 23:26:56 +0100

    Added the logout route

---
+++ src/controllers/auth/signout.ts
@@ -0,0 +1,33 @@
+import { catchAsync } from '@/middlewares';
+import { Response } from 'express';
+import { setCookie, removeFromCache } from '@/common/utils';
+import { UserModel as User } from '@/models';
+import { AppResponse } from '@/common/utils/appResponse';
+import AppError from '@/common/utils/appError';
+import type { CustomRequest } from '@/common/interfaces';
+
+export const signOut = catchAsync(async (req: CustomRequest, res: Response) => {
+	const { user } = req;
+
+	if (!user) {
+		throw new AppError('You are not logged in', 404);
+	}
+
+	const userId = user._id.toString();
+
+	await removeFromCache(userId);
+
+	//$unset the refreshToken from the mongodb
+	await User.findByIdAndUpdate({ _id: userId }, { $unset: { refreshToken: 1 } });
+
+	//you will clear the cookies set on the frontend by setting a new cookie with empty values and an expiry time in the past
+	setCookie(res, 'abegAccessToken', 'expired', {
+		maxAge: -1,
+	});
+
+	setCookie(res, 'abegRefreshToken', 'expired', {
+		maxAge: -1,
+	});
+
+	AppResponse(res, 200, null, 'Logout successful');
+});
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,4 +1,4 @@
-import { protect, forgotPassword, resetPassword, session, signIn, signUp } from '@/controllers';
+import { protect, forgotPassword, resetPassword, session, signIn, signUp, signOut } from '@/controllers';
 import { Router } from 'express';
 
 const router = Router();
@@ -10,5 +10,6 @@ router.post('/password/reset', resetPassword);
 
 router.use(protect); // Protect all routes after this middleware
 router.get('/session', session);
+router.get('/signout', signOut);
 
 export { router as authRouter };
