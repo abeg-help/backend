460484abb9bb8365d2c36092cce88e709fc6fd5e
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-11-25 17:17:35 +0100

    Changed the route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,5 +1,4 @@
 import { protect, forgotPassword, resetPassword, session, signIn, signUp } from '@/controllers';
-import { editUserProfile } from '@/controllers/auth/editUserProfile';
 import { Router } from 'express';
 
 const router = Router();
@@ -11,6 +10,5 @@ router.post('/password/reset', resetPassword);
 
 router.use(protect); // Protect all routes after this middleware
 router.get('/session', session);
-router.get('/user/updateProfile', editUserProfile);
 
 export { router as authRouter };
