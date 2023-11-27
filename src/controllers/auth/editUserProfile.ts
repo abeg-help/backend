a66940e1279859a76817fb5d9e530e2448a96da5
Olaniyi Seyi <Hydradaboss06@gmail.com>
2023-11-27 10:18:41 +0100

    Final changes to update user controller

--- src/controllers/auth/editUserProfile.ts
+++ src/controllers/auth/editUserProfile.ts
@@ -4,13 +4,15 @@ import { catchAsync } from '@/middlewares';
 import { UserModel as User } from '@/models';
 import { CustomRequest, IUser } from '@/common/interfaces';
 import { AppResponse } from '@/common/utils/appResponse';
+import AppError from '@/common/utils/appError';
 
 export const editUserProfile = catchAsync(async (req: CustomRequest, res: Response) => {
 	//collect the details to be updated
 	const { firstName, lastName, phoneNumber, gender } = req.body;
 
 	//get the user id to update from req.user
-	const UserToUpdateEmail = req.user?._id;
+	const UserToUpdateID = req.user?._id;
+	if (!UserToUpdateID) throw new AppError('Provide User ID to updated', 400);
 
 	//Partial makes the objects to update optional while extending the user inteface
 	const objectToUpdate: Partial<IUser> = {
@@ -21,7 +23,7 @@ export const editUserProfile = catchAsync(async (req: CustomRequest, res: Respon
 	};
 
 	//updates the id with object, new returns the updated user while runnung mongoose validation
-	const updatedUser = await User.findByIdAndUpdate({ _id: UserToUpdateEmail }, objectToUpdate, {
+	const updatedUser = await User.findByIdAndUpdate({ _id: UserToUpdateID }, objectToUpdate, {
 		new: true,
 		runValidators: true,
 	});
@@ -30,6 +32,7 @@ export const editUserProfile = catchAsync(async (req: CustomRequest, res: Respon
 		//  no user is found to update
 		return AppResponse(res, 404, null, 'User not found for update');
 	}
-	await setCache(`Updated User: ${updatedUser?._id.toString()}`, { ...toJSON(updatedUser, ['password']) }, 3600);
+
+	await setCache(`Updated User: ${updatedUser?._id.toString()}`, toJSON(updatedUser), 3600);
 	AppResponse(res, 200, updatedUser, 'Profile Successfully Updated');
 });
