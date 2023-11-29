548ea4e4737d99069588c8c5a354b6b8690b5a2c
JC-Coder <josephchimezie2003@gmail.com>
2023-11-29 22:56:53 +0100

    update codebase

--- src/controllers/user/updateProfilePhoto.ts
+++ src/controllers/user/updateProfilePhoto.ts
@@ -2,18 +2,20 @@ import { catchAsync } from '@/middlewares';
 import { CustomRequest, IUser } from '@/common/interfaces';
 import { Response } from 'express';
 import AppError from '@/common/utils/appError';
-import { AppResponse, deleteFile, getFromCache, setCache, toJSON, uploadSingleFile } from '@/common/utils';
+import { AppResponse, getFromCache, setCache, toJSON, uploadSingleFile } from '@/common/utils';
 import { DateTime } from 'luxon';
 import { UserModel } from '@/models';
 
 export const updateProfilePhoto = catchAsync(async (req: CustomRequest, res: Response) => {
 	const { file } = req;
+	const userId = req.user?._id;
 
 	if (!file) {
 		throw new AppError(`File is required`, 400);
 	}
 
-	const fileName = `profile-photos/${DateTime.now().toMillis()}.${file.mimetype.split('/')[1]}`;
+	const dateInMilliseconds = DateTime.now().toMillis();
+	const fileName = `${userId}/profile-image/${userId}-${dateInMilliseconds}.${file.mimetype.split('/')[1]}`;
 
 	const uploadedFile = await uploadSingleFile({
 		fileName,
@@ -22,24 +24,23 @@ export const updateProfilePhoto = catchAsync(async (req: CustomRequest, res: Res
 	});
 
 	const updatedUser = (await UserModel.findByIdAndUpdate(
-		req.user?._id,
+		userId,
 		{
-			$set: {
-				photo: uploadedFile,
-			},
+			photo: uploadedFile,
 		},
 		{ new: true }
-	).select('+photo +refreshToken')) as IUser & { _id: string };
+	)) as IUser & { _id: string };
 
 	// delete previous photo from bucket
 	const userFromCache = await getFromCache(updatedUser._id);
-	if (userFromCache) {
-		const splitItem = Object(userFromCache).photo.split('/');
-		await deleteFile(`profile-photos/${splitItem[splitItem.length - 1]}`);
-	}
 
-	// update cache
-	await setCache(updatedUser._id.toString()!, { ...toJSON(updatedUser), refreshToken: updatedUser.refreshToken });
+	if (userFromCache) {
+		// update cache
+		await setCache(updatedUser._id.toString()!, {
+			...toJSON(updatedUser),
+			refreshToken: Object(userFromCache)?.refreshToken,
+		});
+	}
 
 	return AppResponse(res, 200, updatedUser, 'Profile photo updated successfully');
 });
