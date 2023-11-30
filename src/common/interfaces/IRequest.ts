b0e1e82d95754275382d2a8a2d3374caae1ed97e
1finedev <obcbeats@gmail.com>
2023-11-30 18:39:40 +0300

    rebase

--- src/common/interfaces/IRequest.ts
+++ src/common/interfaces/IRequest.ts
@@ -1,4 +1,5 @@
 import { Require_id } from 'mongoose';
+import Multer from 'multer';
 import { Server } from 'socket.io';
 import { IUser } from './user';
 
@@ -6,5 +7,6 @@ declare module 'express-serve-static-core' {
 	interface Request {
 		user?: Require_id<IUser>;
 		io: Server;
+		file?: Multer.File;
 	}
 }
