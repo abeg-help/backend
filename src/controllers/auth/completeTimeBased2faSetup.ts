3f4448f3183d5e7dbd1685d620daaf3eb10e4d3d
JC-Coder <josephchimezie2003@gmail.com>
2023-12-03 06:01:49 +0100

    update

--- src/controllers/auth/completeTimeBased2faSetup.ts
+++ src/controllers/auth/completeTimeBased2faSetup.ts
@@ -1,4 +1,6 @@
-import { validateTimeBased2fa, AppResponse } from '../../common/utils';
+import { Require_id } from 'mongoose';
+import { IUser } from '../../common/interfaces';
+import { validateTimeBased2fa, AppResponse, getFromCache, setCache, toJSON } from '../../common/utils';
 import AppError from '../../common/utils/appError';
 import { catchAsync } from '../../middlewares';
 import { UserModel } from '../../models';
@@ -10,6 +12,10 @@ export const completeTimeBased2fa = catchAsync(async (req: Request, res: Respons
 
 	if (!token) {
 		throw new AppError('Token is required', 400);
+	}
+
+	if (user?.timeBased2FA.active) {
+		throw new AppError('2FA is already active', 400);
 	}
 
 	if (!user?.timeBased2FA.secret) {
@@ -28,6 +34,13 @@ export const completeTimeBased2fa = catchAsync(async (req: Request, res: Respons
 			'timeBased2FA.secret': 1,
 		},
 	});
+
+	const userFromCache = await getFromCache<Require_id<IUser>>(user._id.toString());
+
+	if (userFromCache) {
+		// update cache
+		await setCache(user._id.toString()!, toJSON({ ...userFromCache, timeBased2FA: { secret: '', active: true } }, []));
+	}
 
 	return AppResponse(res, 200, null, '2FA enabled successfully');
 });
