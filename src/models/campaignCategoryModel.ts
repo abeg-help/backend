d1cc4d63d20e84ddc9e892dba226e5cba9b6a3cb
masako29 <49831574+1finedev@users.noreply.github.com>
2024-02-12 16:48:26 +0300

    fix

--- src/models/campaignCategoryModel.ts
+++ src/models/campaignCategoryModel.ts
@@ -14,6 +14,18 @@ const campaignCategorySchema = new mongoose.Schema<ICampaignCategory>(
 	{ timestamps: true }
 );
 
+// only pick campaigns that are not deleted or suspended
+campaignCategorySchema.pre(/^find/, function (this: Model<campaignCategoryModel>, next) {
+	// pick deleted campaigns if the query has isDeleted
+	if (Object.keys(this['_conditions']).includes('isDeleted')) {
+		this.find({});
+		return next();
+	}
+
+	// do not select campaigns that are deleted or suspended
+	this.find({ isDeleted: { $ne: true } });
+	next();
+});
 export const campaignCategoryModel =
 	(mongoose.models.CampaignCategory as campaignCategoryModel) ||
 	mongoose.model('CampaignCategory', campaignCategorySchema);
