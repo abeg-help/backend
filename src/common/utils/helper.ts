2257ed1bc30ac5e06f55dc867bc0677beb924489
masako29 <49831574+1finedev@users.noreply.github.com>
2024-03-03 22:13:48 +0300

    add cookie partition

--- src/common/utils/helper.ts
+++ src/common/utils/helper.ts
@@ -70,7 +70,7 @@ const decodeData = async (token: string, secret?: string) => {
 };
 
 const setCookie = (res: Response, name: string, value: string | number, options: CookieOptions = {}) => {
-	res.cookie(name, value, {
+	res.cookie('__Host-' + name, value, {
 		httpOnly: true,
 		secure: ENVIRONMENT.APP.ENV === 'production',
 		path: '/',
