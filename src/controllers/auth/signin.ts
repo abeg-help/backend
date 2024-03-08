e2f34745e401ed56225058820e98b574b3976da1
masako29 <49831574+1finedev@users.noreply.github.com>
2024-03-09 01:44:48 +0300

    update

--- src/controllers/auth/signin.ts
+++ src/controllers/auth/signin.ts
@@ -12,7 +12,7 @@ import {
 	toJSON,
 } from '@/common/utils';
 import { catchAsync } from '@/middlewares';
-import { UserModel, campaignModel } from '@/models';
+import { UserModel } from '@/models';
 import { locationModel } from '@/models/LocationModel';
 import { addEmailToQueue } from '@/queues';
 import type { Request, Response } from 'express';
@@ -127,7 +127,7 @@ export const signIn = catchAsync(async (req: Request, res: Response) => {
 			});
 		}
 
-		const campaigns = await campaignModel.find({ creator: user._id }).sort({ createdAt: -1 }).limit(10);
-		return AppResponse(res, 200, { user: toJSON(updatedUser), campaigns }, 'Sign in successful');
+		// const campaigns = await campaignModel.find({ creator: user._id }).sort({ createdAt: -1 }).limit(10);
+		return AppResponse(res, 200, toJSON(updatedUser), 'Sign in successful');
 	}
 });
