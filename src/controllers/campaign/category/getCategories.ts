ed0d06a052e827e87fcc2b158fd4ec8872446f2b
masako29 <49831574+1finedev@users.noreply.github.com>
2024-03-26 14:18:08 +0300

    add count to campaign categories

--- src/controllers/campaign/category/getCategories.ts
+++ src/controllers/campaign/category/getCategories.ts
@@ -4,7 +4,49 @@ import { campaignCategoryModel } from '@/models';
 import { Request, Response } from 'express';
 
 export const getCategories = catchAsync(async (req: Request, res: Response) => {
-	const categories = await campaignCategoryModel.find();
+	let categories = await campaignCategoryModel.aggregate([
+		{
+			$lookup: {
+				from: 'campaigns',
+				localField: '_id',
+				foreignField: 'category',
+				as: 'campaigns',
+			},
+		},
+		{
+			$addFields: {
+				count: { $size: '$campaigns' },
+			},
+		},
+		{
+			$project: {
+				campaigns: 0,
+			},
+		},
+	]);
+
+	categories = categories.map((category) => {
+		const count = category.count;
+		let formattedCount = '';
+		if (count >= 1000000000000) {
+			formattedCount = Math.floor(count / 100000000000) / 10 + 't';
+		} else if (count >= 1000000000) {
+			formattedCount = Math.floor(count / 100000000) / 10 + 'b';
+		} else if (count >= 1000000) {
+			formattedCount = Math.floor(count / 100000) / 10 + 'm';
+		} else if (count >= 1000) {
+			formattedCount = Math.floor(count / 100) / 10 + 'k';
+		} else {
+			formattedCount = count.toString();
+		}
+
+		// Add + if count is not a multiple of 1000 and is greater than or equal to 1000
+		if (count % 1000 > 0 && count >= 1000) {
+			formattedCount += '+';
+		}
+
+		return { ...category, count: formattedCount };
+	});
 
 	return AppResponse(res, 200, categories, 'Success');
 });
